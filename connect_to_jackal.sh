60b6d0b4c21d311a1bbde2314c0503ec6f0b59ce
juleszwanen <jzwanen@hotmail.com>
2025-10-24 14:07:02 +0200

    The added python package should allow for communication between 2 rosmaster and the changed connect_to_ackal.sh now contains the correct connection parameters for each jackal

--- connect_to_jackal.sh
+++ connect_to_jackal.sh
@@ -3,8 +3,14 @@ source devel/setup.bash
 # Jackal 1
 # export ROS_MASTER_URI=http://cpr-j100-0114:11311
 
+#Jackal 2 -> the rosmaster is on port 11331
+export ROS_MASTER_URI=http://192.168.0.102:11311
+
 # Jackal 3
-export ROS_MASTER_URI=http://cpr-j100-0594:11311
+# export ROS_MASTER_URI=http://cpr-j100-0594:11311
+
+#Jackal 4 -> the rosmaster is on port 11331
+export ROS_MASTER_URI=http://192.168.0.104:11311
 
 # Your IP (check with `ip a`)
-export ROS_IP=192.168.3.56
+export ROS_IP=192.168.0.99
