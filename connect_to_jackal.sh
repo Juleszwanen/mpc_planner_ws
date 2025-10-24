63ea62dda71eb994c1ab38d79fea7833503f3939
juleszwanen <jzwanen@hotmail.com>
2025-10-24 15:58:29 +0200

    added the guidance_planner_multi_robot_nodes

--- connect_to_jackal.sh
+++ connect_to_jackal.sh
@@ -10,7 +10,7 @@ export ROS_MASTER_URI=http://192.168.0.102:11311
 # export ROS_MASTER_URI=http://cpr-j100-0594:11311
 
 #Jackal 4 -> the rosmaster is on port 11331
-export ROS_MASTER_URI=http://192.168.0.104:11311
+# export ROS_MASTER_URI=http://192.168.0.104:11311
 
 # Your IP (check with `ip a`)
 export ROS_IP=192.168.0.99
